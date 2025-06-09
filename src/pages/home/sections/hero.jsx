9bbf2a975b9bf30fe10f2d07fe6644467cb33824
Maha Abo Assi <maha@onekw.com>
2025-06-09 23:13:26 +0400

    fix data declare

--- src/pages/home/sections/hero.jsx
+++ src/pages/home/sections/hero.jsx
@@ -6,8 +6,8 @@ import OffersProviders from "../../../components/bestOffers"
 import { useTranslation } from "react-i18next"
 
 const Hero = ()=>{
-    const data = []
-    const datam = [{
+
+    const data = [{
         icon : <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 22 22" fill="none">
             <circle cx="11" cy="11" r="11" fill="#872121"/>
             </svg>,
